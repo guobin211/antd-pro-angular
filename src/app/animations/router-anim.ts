c61d125ed278be7d20e9364327cd7692e210baa1
GuoBin <guobin201314@gmail.com>
2020-07-07 11:40:06 +0800

    prefer(animations)

--- src/app/animations/router-anim.ts
+++ src/app/animations/router-anim.ts
@@ -1,17 +1,17 @@
-import { trigger, state, transition, style, animate, group, query } from '@angular/animations';
+import { trigger, state, transition, style, animate, group, query } from "@angular/animations";
 
-export const routerAnimation = trigger('routerAnimation', [
-  transition(':enter', [
-    style({position: 'absolute'}),
-    animate('0.5s ease-in-out')
+export const routerAnimation = trigger("routerAnimation", [
+  transition(":enter", [
+    style({position: "absolute"}),
+    animate("0.5s ease-in-out")
   ]),
-  transition('* => *', [
-    query(':leave', style({ transform: 'translateX(0)', position: 'absolute'}), { optional: true }),
-    query(':enter', style({ transform: 'translateX(100%)', position: 'absolute'}), { optional: true }),
+  transition("* => *", [
+    query(":leave", style({ transform: "translateX(0)", position: "absolute"}), { optional: true }),
+    query(":enter", style({ transform: "translateX(100%)", position: "absolute"}), { optional: true }),
 
     group([
-      query(':leave', animate('.5s ease-in-out', style({transform: 'translateX(-100%)'})), { optional: true }),
-      query(':enter', animate('.5s ease-in-out', style({transform: 'translateX(0)'})), { optional: true })
+      query(":leave", animate(".5s ease-in-out", style({transform: "translateX(-100%)"})), { optional: true }),
+      query(":enter", animate(".5s ease-in-out", style({transform: "translateX(0)"})), { optional: true })
     ])
   ])
 ]);
