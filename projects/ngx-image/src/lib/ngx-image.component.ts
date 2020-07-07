fccfb6b59ff0e724faba9884296e5752d0992dbe
GuoBin <guobin201314@gmail.com>
2020-07-07 11:40:41 +0800

    style(app)

--- projects/ngx-image/src/lib/ngx-image.component.ts
+++ projects/ngx-image/src/lib/ngx-image.component.ts
@@ -1,14 +1,14 @@
 import {
   Component,
   Input,
-} from '@angular/core';
+} from "@angular/core";
 
 export type NgxImageProps = keyof HTMLImageElement;
 
 @Component({
-  selector: 'ngx-image',
-  templateUrl: './ngx-image.html',
-  styleUrls: ['./ngx-image.css'],
+  selector: "ngx-image",
+  templateUrl: "./ngx-image.html",
+  styleUrls: ["./ngx-image.css"],
 })
 export class NgxImageComponent {
   @Input() props: NgxImageProps;
--- projects/ngx-image/src/public-api.ts
+++ projects/ngx-image/src/public-api.ts
@@ -1,5 +1,5 @@
 /*
  * Public API Surface of ngx-image
  */
-export * from './lib/ngx-image.component';
-export * from './lib/ngx-image.module';
+export * from "./lib/ngx-image.component";
+export * from "./lib/ngx-image.module";
